c13b427aef6e027941570b574646515c01276d95
hsiu chen <166912934+hsiu2701@users.noreply.github.com>
2025-03-26 08:31:12 +0800

    修改圖片

--- src/pages/CheckoutFormPage.jsx
+++ src/pages/CheckoutFormPage.jsx
@@ -117,7 +117,8 @@ export default function Checkout() {
         <div
           className="checkout-bannerImg"
           style={{
-            backgroundImage: 'url("./banner.png")',
+            backgroundImage:
+              'url("https://images.unsplash.com/photo-1415369629372-26f2fe60c467?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
             backgroundSize: "cover",
             backgroundPosition: "center",
           }}
--- src/pages/CheckoutSuccessPage.jsx
+++ src/pages/CheckoutSuccessPage.jsx
@@ -81,7 +81,8 @@ export default function CheckoutSuccess() {
       <div
         className="order-success-banner"
         style={{
-          backgroundImage: 'url("./banner.png")',
+          backgroundImage:
+            'url("https://images.unsplash.com/photo-1415369629372-26f2fe60c467?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
